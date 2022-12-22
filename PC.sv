0a0256cd9d34d7daee85120d84b25640631d3947
Fatemeh Davoudi <70088942+davoudifatemeh@users.noreply.github.com>
2022-12-22 14:49:23 +0330

    Delete PC.sv

--- PC.sv
+++
@@ -1,9 +0,0 @@
-`timescale 1ns/1ns
-module PC(input clk, rst, en, input[31:0] pcIn, output logic[31:0] pcOut);
-  always @(posedge clk, posedge rst) begin
-    if (rst)
-      pcOut <= 32'b0;
-    else if (en)
-      pcOut <= pcIn;
-  end
-endmodule
